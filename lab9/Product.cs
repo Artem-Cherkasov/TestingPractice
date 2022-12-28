521597b235724c9e980c055fb1c07875243d9e0e
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2022-12-28 22:58:13 +0300

    Lab9

---
+++ lab9/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Entities
+{
+    public class Product
+    {
+        public int? id { get; set; }
+        public int? category_id { get; set; }
+        public string? title { get; set; }
+        public string? alias { get; set; }
+        public string? content { get; set; }
+        public int? price { get; set; }
+        public int? old_price { get; set; }
+        public int? status { get; set; }
+        public string? keywords { get; set; }
+        public string? description { get; set; }
+        public int? hit { get; set; }
+    }
+}
---
+++ lab9/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab9.Entities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Unidecode.NET;
+using NickBuhro.Translit;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var a = Transliteration.CyrillicToLatin("Итали", Language.Russian).ToLower();
+            var b = "itali";
+
+
+            Console.WriteLine(a) ;     */       
+        }
+    }
+}
